e9e72e00dbe17c724aaabba1289dc61754918b9f
forestser <forester00@gmail.com>
2022-01-14 13:07:16 +0900

    first

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('닉네임 변경'),
+      ),
+    );
+  }
+  
+}
